891aa04a5ce7a45cba5a63b107ad3f369156bdf5
harsh katharotiya <105359856+harshkathrotiya@users.noreply.github.com>
2024-07-24 16:41:05 +0530

    Create test.cpp

---
+++ cp/test.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main(){
+    
+   string s;
+   cin>>s;
+   string s1;
+   cin>>s1;
+   if(s==s1)
+    cout<<"yes";
+    else
+    cout<<"No";
+
+    
+    
+    // return 0;
+
+}
